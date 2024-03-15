e66cca14ead7bec43a1b4b2c1c2462ec3982e58f
kjrstory <kjrstory@gmail.com>
2024-03-15 14:05:44 +0000

    temp revert

--- assets/js/bookshelf.js
+++ assets/js/bookshelf.js
@@ -1,32 +1,3 @@
-function getRootCssStyles(rootRule = ":root") {
-  // Get all CSS rules for the document using Array methods
-  const cssRulesArray = [...document.styleSheets]
-      .map(styleSheet => {
-          try {
-              return [...styleSheet.cssRules]
-                  .map(rule => rule)
-          } catch (e) {
-              // console.log('Access to stylesheet %s is denied. Ignoring...', styleSheet.href);
-          }
-      })
-
-  var cssVars = [];
-  // Get custom styles from root css rule 
-  Object.values(cssRulesArray).forEach(arrayElement => {
-      Object.values(arrayElement).forEach(ruleElement => {
-          if (ruleElement.selectorText === rootRule) {
-              Object.values(ruleElement.style).forEach(style => {
-                  if (style.startsWith('--spine-') && cssVars.indexOf(style) == -1) {
-                      cssVars.push(style);
-                  }
-              })
-          }
-      })
-  })
-
-  return cssVars;
-}
-
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -41,7 +12,7 @@ let spines = Object.values(document.getElementsByClassName("spine"));
 let covers = Object.values(document.getElementsByClassName("cover"));
 let tops = Object.values(document.getElementsByClassName("top"));
 
-let availablePatterns = getRootCssStyles();
+let availablePatterns = ["--spine-pyramid", "--spine-stairs", "--spine-argyle", "--spine-tartan"];
 
 let availableColors = [
   "maroon",
